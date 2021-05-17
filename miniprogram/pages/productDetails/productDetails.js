211e65fcf2da2c4a30bd43bdfce87e6084b1203f
xy1327462182 <1327462182@qq.com>
2021-05-17 17:07:31 +0800

    商品详情页1部分

---
+++ miniprogram/pages/productDetails/productDetails.js
@@ -0,0 +1,71 @@
+// pages/productDetails/productDetails.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imageList: [
+      'https://img0.baidu.com/it/u=932905319,1056343345&fm=224&fmt=auto&gp=0.jpg',
+      'https://img0.baidu.com/it/u=416937946,2549283029&fm=11&fmt=auto&gp=0.jpg',
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi1.go2yd.com%2Fimage.php%3Furl%3D0U5bgEkI7M&refer=http%3A%2F%2Fi1.go2yd.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1623830711&t=b00085c26f68828a58189730305d1170',
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi1.go2yd.com%2Fimage.php%3Furl%3D0U5bgEk2s0&refer=http%3A%2F%2Fi1.go2yd.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1623830722&t=62ea03357e857cd0218c1f5614dc09d6'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
